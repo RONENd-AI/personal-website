990b69d10ae8348e50178147044ea7c80838da23
Dinkar khergade <dinkar.khergade1995@live.com>
2024-10-21 23:41:05 +0530

    Your commit message here

---
+++ script.js
@@ -0,0 +1,7 @@
+<header>
+    <div class="container">
+        <img src="images/profile.jpg" alt="Dinkar Khergade" width="200"/>
+        <h1>Dinkar Khergade</h1>
+        <p>Linux System Administrator | RedHat | AWS | PostgreSQL</p>
+    </div>
+</header>
